d0a459513c699643425ad185edaaa4084200d85e
Onohije <igbadumeonoh@gmail.com>
2024-01-04 14:02:06 -0800

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Computes Pascal Triangle"""
+
+
+def pascal_triangle(n):
+    """Computes Pascal Triangle"""
+    result = []
+    if n <= 0:
+        return result
+    for i in range(n):
+        if len(result) == 0:
+            result.append([1])
+        else:
+            row = [1]
+            for j in range(1, len(result[-1])):
+                row.append(result[-1][j] + result[-1][j - 1])
+            row.append(1)
+            result.append(row)
+    return result

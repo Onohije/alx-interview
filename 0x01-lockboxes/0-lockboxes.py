cecbe5f9e3da19b895a18ec67f9af8e1beb6f9ae
Onohije <igbadumeonoh@gmail.com>
2024-01-09 13:51:47 -0800

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,24 +1,33 @@
-#!/usr/bin/python3
-"""0x01. Lockboxes"""
-
-
 def canUnlockAll(boxes):
-    """a function that determines if all the
-    boxes can be opened"""
-    keys = set([0])
-    unlocked = []
-    boxes_copy = boxes[:]
+    if not boxes or len(boxes) == 0:
+        return False
 
-    while True:
-        try:
-            key = keys.pop()
-            keys.update(boxes_copy[key])
-            unlocked.append(boxes_copy[key])
-            boxes_copy[key] = []
-        except KeyError:
-            break
+    # Initialize a set to keep track of visited boxes
+    visited = set()
+    visited.add(0)  # Mark the first box as visited
 
-    for box in boxes:
-        if box not in unlocked:
-            return False
-    return True
+    # Initialize a stack to perform depth-first search
+    stack = [0]
+
+    # Perform depth-first search to check if all boxes can be opened
+    while stack:
+        current_box = stack.pop()
+
+        # Iterate through the keys in the current box
+        for key in boxes[current_box]:
+            if key not in visited and key < len(boxes):
+                stack.append(key)
+                visited.add(key)
+
+    # Check if all boxes have been visited
+    return len(visited) == len(boxes)
+
+# Example usage:
+boxes1 = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes1))  # Output: True
+
+boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes2))  # Output: True
+
+boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes3))  # Output: False

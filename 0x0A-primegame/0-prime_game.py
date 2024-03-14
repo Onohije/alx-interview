167505de327af5c23c638a12b6941431d8b5d084
Onohije <igbadumeonoh@gmail.com>
2024-03-14 15:49:40 -0700

    primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,36 +1,29 @@
 #!/usr/bin/python3
-"""0. The Prime Game"""
+""" Module for solving prime game question """
 
+def isWinner(x, nums):
+    """function that checks for the winner"""
+    if not nums or x < 1:
+        return None
+    max_num = max(nums)
 
-def isWinner(x: int, nums: list) -> str:
-    """determine who the winner of each game is between Ben and Maria"""
-    #  print('x:', x, 'nums:', nums)
-    ben = 0
-    maria = 0
-
-    for round in range(x):
-        arr = list(range(1, nums[round] + 1))
-        #  print('Round', round + 1, ':', arr)
-
-        index = 0
-        while len(arr) > 1:
-            index += 1
-            #  print('Maria\'s' if index % 2 == 1 else 'Ben\'s', 'move:', arr)
-            prime = arr[1]
-            for num in arr[:]:
-                if num % prime == 0:
-                    arr.pop(arr.index(num))
-        if index % 2 == 1:
-            maria += 1
-            #  print('Maria won!', '>>>', 'Ben:', ben, 'Maria:', maria)
-        else:
-            ben += 1
-            #  print('Ben won!', '>>>', 'Ben:', ben, 'Maria:', maria)
-
-    #  print('Ben:', ben, 'Maria:', maria)
-    # return results
-    if ben > maria:
-        return 'Ben'
-    elif maria > ben:
-        return 'Maria'
-    return None
+    my_filter = [True for _ in range(max(max_num + 1, 2))]
+    for i in range(2, int(pow(max_num, 0.5)) + 1):
+        if not my_filter[i]:
+            continue
+        for j in range(i * i, max_num + 1, i):
+            my_filter[j] = False
+    my_filter[0] = my_filter[1] = False
+    y = 0
+    for i in range(len(my_filter)):
+        if my_filter[i]:
+            y += 1
+        my_filter[i] = y
+    player1 = 0
+    for x in nums:
+        player1 += my_filter[x] % 2 == 1
+    if player1 * 2 == len(nums):
+        return None
+    if player1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
